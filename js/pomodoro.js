35b4475803897b5f7a4f353bf5e30d61f61fcc16
Leneli <lena@realrussia.co.uk>
2016-03-16 14:15:36 +0300

    Timer v.1

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,99 +1,101 @@
-var doc = document,
-	pomodoro = doc.getElementsByClassName('pomodoro'), //array, div.pomodoro
-	minute = doc.getElementById('minute'),
-	second = doc.getElementById('second'),
+var pomodoro = document.getElementsByClassName('pomodoro'),
+	minute = document.getElementById('minute'),
+	second = document.getElementById('second'),
+	popup = document.getElementById('popup'),
+	message = document.getElementById('message'),
+	ok = document.getElementById('ok'),
 	timeWork = 1500000, //25m
 	timeCoffee = 300000, //5m
-	active = 'active', //таймер включен, время работы
+	active = 'active', //таймер включен, время работать
 	coffee = 'coffee', //таймер включен, время на кофе
-	i = 0; //счетчик кликов по таймеру
-
-//Добавление/удаление классов
-function addCoffe() {
-	pomodoro[0].classList.add(coffee);
-}
-
-function removeCoffe() {
-	pomodoro[0].classList.remove(coffee);
-}
-
-function addActive() {
-	pomodoro[0].classList.add(active);
-}
-
-function removeActive() {
-	pomodoro[0].classList.remove(active);
-}
-
-
-//Вывод времени (1 min = 60 000 ms) ????????
-function addTime(t) {
-	var t0, t1, t2;
-	
-	for(t; t > 0; t - 1000) {
-		t0 = t / 60000;
-		t0 = t0.toFixed(2).toString().split('.');
-		t1 = parseInt(t0[0], 10); //минуты
-		t2 = parseFloat(t0[1]*0.6, 10).toFixed(); //секунды
-	//	
-		console.log(t);
-		console.log(t0);
-		console.log(t1);
-		console.log(t2);
-	//
-
-		if(t1 >= 10) {
-			minute.innerHTML = t1;
-		} else {
-			minute.innerHTML = '0' + t1;
-		}
-
-		if(t2 >= 10) {
-			second.innerHTML = t2;
-		} else {
-			second.innerHTML = '0' + t2;
-		}
-	}
-	
-	
-
-	
-	console.log(t);
-	
-	//setTimeout(addTime(t), 1000);
-}
+	i = 0, //счетчик кликов по таймеру
+	intervalID; 
 
 
 //Таймер
-pomodoro[0].onclick = function() {
-	//addTime(timeWork);
-	
+pomodoro[0].onclick = function() {	
 	++i;
+	
 	if (i%2 !== 0) { //по первому клику - таймер [пере]запущен
-		addActive();
+		pomodoro[0].classList.add('active');
+		startTimer(5000);
 		
-		addTime(10000);
-		
-		/*if(timeWork === 0) {
-			removeActive();
-			addCoffe();
-		}
-		addTime(timeCoffee);
-		
-		if(timeCoffee === 0) {
-			removeCoffe;
-		}*/
+		message.innerHTML = 'Coffee break!';
+		ok.onclick = function() {
+			pomodoro[0].classList.remove('active');
+			pomodoro[0].classList.add('coffee');
+			popup.classList.remove('block');
+			
+			message.innerHTML = "It's time to work!";
+			startTimer(3000);
+			ok.onclick = function() {
+				pomodoro[0].classList.remove('active');
+				pomodoro[0].classList.remove('coffee');
+				printTime(timeWork);
+				popup.classList.remove('block');
+				
+				i = 0; //обнуление счетчика кликов
+			};
+		};
 	} 
+	
 	else {			 //по второму клику - таймер отключен
-		removeCoffe();
-		removeActive();
+		pomodoro[0].classList.remove('active');
+		pomodoro[0].classList.remove('coffee');
+		clearInterval(intervalID);
+		printTime(timeWork);
+	}
+};
+
+
+
+
+
+
+
+
+
+//**************************** ГОТОВЫЕ РАБОТАЮЩИЕ ФУНКЦИИ
+
+function startTimer (t) { //отсчет времени
+	printTime(t);
+	
+	intervalID = setInterval(function() {
+		t -= 1000;
+		printTime(t);
+
+		if(t < 1000) {
+			clearInterval(intervalID);
+			popup.classList.add('block');
+		};
+		
+	}, 1000);
+};
+
+
+//Вывод времени на экран
+function printTime(t) {
+	var min, sec;
+	
+	t /=1000;
+	
+	if(t < 60) {
+		min = 0;
+		sec = t;
+	} else {
+		min = Math.floor(t/60);
+		sec = t - (min*60);
 	}
 	
-}
-
-
-
-
-
-
-
+	if(min < 10) {
+		minute.innerHTML = '0' + min;
+	} else {
+		minute.innerHTML = min;
+	}
+	
+	if(sec < 10) {
+		second.innerHTML = '0' + sec;
+	} else {
+		second.innerHTML = sec;
+	}
+};
